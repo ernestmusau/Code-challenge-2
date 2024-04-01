c3cec6ade05f026d323be4d500d5db9dc22ed0d3
ernestmusau <ernest.musau@student.moringaschool.com>
2024-04-01 22:02:27 +0300

    initial commit

---
+++ question2.js
@@ -0,0 +1,11 @@
+function generateArray(start, end) {
+    let array = [];
+    for (let i = start; i <= end; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+  
+  // Example usage:
+  console.log(generateArray(4, 7)); // Output: [4, 5, 6, 7]
+  console.log(generateArray(-4, 7)); // Output: [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
